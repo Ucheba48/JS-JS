18c8c801ffb1a5e3f7889296d3a7f3ea40ded601
Ucheba48 <obuchenie48@gmail.com>
2023-04-14 17:32:02 +0300

    massiv

--- script.js
+++ script.js
@@ -22,31 +22,98 @@ if (num > 20) {
 } else {
     console.log('The num is more than 10')
 }
-
-let age = 17 
+/*
+let age = 15 
 // < 16 boy
 // 16 <21 young man
 // 21 < Mr.
-let gender = 'Male'
+let gender = 'Female' // 'Male'
 
-let prefix
+let prefix // boy | Mr. | girl |Mrs.
 
 if ((age > 18) && (gender == 'Male')) {
     prefix = 'Mr.'
-} else if ((age > 18) && (gender == 'Famale')){
+ } else if ((age > 18) && (gender == 'Famale')){
     prefix = 'Mrs'
-} else if ((age <= 18) && (gender == 'Male')) {
+ } else if ((age <= 18) && (gender == 'Male')) {
     prefix = 'boy'
-} else if ((age <= 18) && (gender == 'Famale')) {
+ } else if ((age <= 18) && (gender == 'Famale')) {
     prefix = 'girl'
-}
-if (age = 18) {
+ }
+ if (age > 18) {
     if (gender == 'Male') {
         prefix = 'Mr'
     } else {
         prefix = 'Mrs.'
     }
-} else {
-    if (gender == 'Male')
+ } else {
+    if (gender == 'Male') {
+        prefix = 'boy'
+    } else {
+        prefix = 'girl'
+    }
+ }
+ prefix = (age > 18)
+    ? (gender == 'Male') ? 'Mr' : 'Mrs.'
+    : (gender == 'Male') ? 'boy' : 'girl' 
+ console.log(prefix);
+ */
+let price
+let city = 'Volgograd'
+
+switch (city) {
+    case 'Moscow':
+        price = 10 
+        break;
+    case 'Berlin':
+        price = 9
+        break;
+    case 'Paris':
+        price = 8
+        break
+    default:
+        price = 15
+        break;
 }
-console.log(prefix);
+console.log('Price: $' + price);
+console.log(`Price (${city}): $ ${price}`);
+
+/*
+let counter = 0
+while (counter < 11) {
+    console.log(counter);
+    counter++
+}
+*/
+/*
+ for (let i = 0; i < 11; i++) {
+    console.log(i);
+ }
+*/
+/*
+let numOfPersons = 26
+let numOfChild = 0
+let numOfAdult = 0
+let age1 = 1
+while (age1 <= numOfPersons) {
+    if (age1 < 18) {
+        numOfChild++
+    } else {
+        numOfAdult++
+    }
+    age1++
+}
+*/
+/*
+for (let age1 = 1; age1 <= numOfPersons; age1++) {
+    if (age1 < 18) {
+        numOfChild++
+    } else {
+        numOfAdult++
+    }
+}
+
+console.log('Num of Child: ' + numOfChild);
+console.log('Num of Adult: ' + numOfAdult);
+*/
+let arrayOfAge = [13, 12, 7, 28, 45, 63, ]
